c05c5599a8e8adc567122da6c2ba331b5165f67d
tega <alessiotegoni4@gmail.com>
2024-10-30 13:28:40 +0100

    [ADD] completed exercises

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,12 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
+
+const peopleNames = people.map(({ name }) => name);
+
+console.log(peopleNames.join(", "));
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
